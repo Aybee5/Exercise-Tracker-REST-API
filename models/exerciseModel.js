39072373f87b547d67c3ec23c8992807e6be9c7f
Ibrahim Abdullahi Aliyu <ibrahiimaa05@gmail.com>
2020-05-12 16:14:30 +0100

    models setup

---
+++ models/exerciseModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    exercise: [{type: Schema.Types.ObjectId, ref:'Exercise'}]
+})
+
+const exerciseSchema = Schema({
+  userId: {
+      type: Schema.Types.ObjectId,
+      ref:'User',
+      required: true,
+  },
+  description: {
+      type: String,
+      required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+      type: Date,
+      default: new Date().toUTCString()
+  }
+})
+
+
+const Exercise = module.exports = mongoose.model("Exercise", exerciseSchema)
+const User = module.exports =  mongoose.model("User", userSchema)
+
+module.exports = {
+  Exercise: Exercise,
+  User: User
+}
